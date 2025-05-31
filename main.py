6342c7b22d03c4c7e0977c1aee4ac4bb83778060
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2025-05-31 15:56:32 -0400

    feat: add extraction for title and level, school, ritual, subschool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """ Tool for having class and level relevant spells depending on context """
-
+import sys
 import os
 import re
 import argparse
@@ -60,9 +60,13 @@ def main():
     spell_files = get_list_of_files(directory)
     spells = []
     for file_path in spell_files:
-        soup = open_html_file(file_path)
-        spell_data = parse_spell_file(soup)
-        spells.append(spell_data)
+        try:
+            soup = open_html_file(file_path)
+            spell_data = parse_spell_file(soup)
+            spells.append(spell_data)
+        except KeyboardInterrupt:
+            print("\nExiting from Ctrl-C...")
+            sys.exit()
 #        return
 
     # for spell in spells:
@@ -91,28 +95,22 @@ schools = {
     "evocation": "Evocation",
     "illusion": "Illusion",
     "necromancy": "Necromancy",
-    "transmutation": "Transmutation"
-}
-regex_schools = re.compile("|".join(schools.keys()), re.IGNORECASE)
-
-extra_schools_data = {
+    "transmutation": "Transmutation",
     "dunamancy": "Dunamancy",
     "dunamancy:chronurgy": "Chronurgy Dunamancy:",
     "dunamancy:graviturgy": "Graviturgy Dunamancy",
     "ritual": "Ritual",
     "technomagic": "Technomagic"
 }
-regex_schools_extra = re.compile("|".join(extra_schools_data.keys()), re.IGNORECASE)
-
-REGEX_ORDINAL_SUFFIXES = r"(?:st|nd|rd|th)"
-
+REGEX_SCHOOLS = "|".join(schools.keys())
+REGEX_EXTRA = r"(?: \(([\w:]+)\))?"
+REGEX_ORDINAL = r"(?:st|nd|rd|th)"
+print(REGEX_SCHOOLS)
 
 regex_dict = {
     "source": r"^Source: ([\w\W]+)$",
-    "level_school_extra_extra": rf"(\d+){REGEX_ORDINAL_SUFFIXES}-level ({regex_schools}) \(({regex_schools_extra})\) \(({regex_schools_extra})\)",
-    "level_school_extra": rf"(\d+){REGEX_ORDINAL_SUFFIXES}-level ({regex_schools}) \(({regex_schools_extra})\)",
-    "level_school": rf"(\d+){REGEX_ORDINAL_SUFFIXES}-level \(({regex_schools})\)",
-    "school_cantrip": r"(\w+)\s cantrip",
+    "level_school": rf"(\d+){REGEX_ORDINAL}-level ([\w]+){REGEX_EXTRA}{REGEX_EXTRA}",
+    "school_cantrip": rf"^({REGEX_SCHOOLS})\s+(cantrip){REGEX_EXTRA}{REGEX_EXTRA}",
     "ritual_or_subschool": r"\s\(([\S]+)\)",  # might be more than one, so please iterate
     "casting_time": r"Casting Time: ([\w\s]+)",
     "range": r"Range: ([\w\W]+)<br ?/>",
@@ -126,7 +124,7 @@ regex_dict = {
 }
 
 
-DEBUG = {}
+DEBUG = {"result"}
 
 
 def get_source(html) -> str:
@@ -140,13 +138,51 @@ def get_source(html) -> str:
     return None
 
 
+def normalize_level_school_result(level_school_result: list) -> list:
+    """ Re-organize result to be level (int), school (string), extra1, extra2"""
+    if not level_school_result:
+        return None
+    level = -1
+    school = ""
+    if level_school_result[1] == 'cantrip':
+        level = 0
+        school = level_school_result[0]
+    elif level_school_result[1] in schools:
+        level = int(level_school_result[0])
+        school = schools[level_school_result[1]]  # fix case for school
+
+    ritual = False
+    subschool = None
+    for x in range(2, 4):  # fix case for extra school stuff
+        current_extra = level_school_result[x]
+        if current_extra == "ritual":
+            ritual = True
+        elif current_extra in schools:
+            subschool = schools[level_school_result[x]]
+
+    if level == -1:
+        return None
+
+    return {"level": level, "school": school, "ritual": ritual, "subschool": subschool}
+
+
 def get_level_and_school_etc(html) -> Tuple[str]:
     """ Parse and return school and level from html """
-    result = re.search(regex_dict["level_school"], html, re.IGNORECASE)
+    result_list = []
+    result = re.search(regex_dict["level_school"], html, flags=re.IGNORECASE)
     if result:
-        level = result.group(1)
-        school = result.group(2)
-        return (level, school)
+        for x in range(1, 5):
+            result_list.append(result.group(x))
+
+    if not result:
+        result = re.search(regex_dict["school_cantrip"], html, flags=re.IGNORECASE)
+        if result:
+            for x in range(1, 5):
+                result_list.append(result.group(x))
+    result_list = normalize_level_school_result(result_list)
+    if "level etc" in DEBUG and result_list:
+        print(result_list)
+    return result_list
 
 
 def strip_tags(html) -> str:
@@ -174,48 +210,26 @@ def parse_spell_file(soup: BeautifulSoup) -> dict:
     for tag in unwrap_list:
         for element in soup.select(tag):
             element.unwrap()
-
+    regex_title = r"^(.+) - DND 5th Edition"
+    title = soup.find("title").get_text()
+    title = re.search(regex_title, title)
+    if title:
+        title = title.group(1)
     page_content = soup.find_all('p')
     # print(get_source(page_content))
-    spell_dict = {}
+    spell_dict = {"title": title}
     for x in page_content:
         str_line = strip_tags(str(x))
 
         source = get_source(str_line)
-        spell_dict["source"] = source
-        print(str_line)
+        if source:
+            spell_dict["source"] = source
+        level_school_etc = get_level_and_school_etc(str_line)
+        if level_school_etc:
+            spell_dict.update(level_school_etc)
 
-    # print(page_content)
-
-    # spell_data['source'] = re.search(r'Source: (.+)', source).group(1)
-
-    # school_level = soup.select_one('div.page-content p em').get_text(strip=True)
-    # match = re.match(r'(\d+)(?:-level)? (\w+)(?: \((ritual)\))?', school_level)
-    # if match:
-    #     spell_data['level'] = int(match.group(1))
-    #     spell_data['school'] = match.group(2).capitalize()
-    #     spell_data['ritual'] = bool(match.group(3))
-
-    # # Extract casting time, range, components, duration
-    # details = soup.select_one('div.page-content p').get_text(strip=True)
-    # details_parts = details.split('\n')
-    # for part in details_parts:
-    #     if 'Casting Time:' in part:
-    #         spell_data['casting_time'] = part.split(':')[1].strip()
-    #     elif 'Range:' in part:
-    #         spell_data['range'] = part.split(':')[1].strip()
-    #     elif 'Components:' in part:
-    #         spell_data['components'] = part.split(':')[1].strip()
-    #     elif 'Duration:' in part:
-    #         spell_data['duration'] = part.split(':')[1].strip()
-
-    # # Extract description
-    # description_paragraphs = soup.select('div.page-content p')[1:]
-    # spell_data['description'] = ' '.join([p.get_text(strip=True) for p in description_paragraphs])
-
-    # # Extract spell lists
-    # spell_lists = soup.select_one('div.page-content p strong em').find_next_siblings('a')
-    # spell_data['spell_lists'] = [a.get_text(strip=True) for a in spell_lists]
+    if "result" in DEBUG:
+        print(spell_dict)
 
     return spell_data
 
